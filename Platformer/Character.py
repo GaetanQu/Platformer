a87f4b6f7f2180a73bd921148c9d115bf844450f
gaetan quenouille <gaetan.quenouille.gq@gmail.com>
2023-05-28 22:57:29 +0200

    résolutions de bugs de collision

--- Platformer/Character.py
+++ Platformer/Character.py
@@ -20,6 +20,7 @@ class Player():
         self.screen = screen
         self.ability_to_move_right = True
         self.ability_to_move_left = True
+        self.on_floor = False
 
 
     def move(self, keys, platforms):    #La methode move ne boucle pas, elle est appelee dans une boucle
@@ -34,11 +35,21 @@ class Player():
             on fait bouger le joueur
         """
 
+        self.on_floor = False
+        for platform in platforms:
+            for i in range(0,self.sprite.get_width()):
+                if platform.rect.collidepoint(self.pos_x + i, self.pos_y+self.sprite.get_height()) and not self.is_jumping:
+                    self.on_floor = True
+                    self.jump_ability = True
+                    self.jump_velocity = 0
+                    self.pos_y -= abs(platform.pos_y - (self.pos_y + self.sprite.get_height()))
+
+
         if keys[pygame.K_d]:
             self.ability_to_move_right = True   #Permet de reset le droit de se deplacer vers la droite, notamment suite a une chute lorsqu'on s'est pris un obstacle volant
             self.ability_to_move_left = True #Si on se deplace vers la droite, par defaut on doit pouvoir se deplacer vers la gauche apres
             for platform in platforms:      #On isole chaque plateforme pour en tester les collisions avec l'objet Player
-                for i in range (0,self.sprite.get_height()):
+                for i in range (1,self.sprite.get_height()):
                     if platform.rect.collidepoint(self.pos_x + self.sprite.get_width(), self.pos_y + i):   #On teste les collisions pour la ligne de droite du rect
                         self.ability_to_move_right = False      #Si il est colle, il peut plus bouger, il suffit d'une seule plateforme
                         self.pos_x -= abs(self.pos_x + self.sprite.get_width() - platform.pos_x)    #On tp le joueur de sorte qu'il traverse pas la plateforme
@@ -49,7 +60,7 @@ class Player():
             self.ability_to_move_left = True
             self.ability_to_move_right = True
             for platform in platforms:
-                for i in range (0, self.sprite.get_height()):
+                for i in range (1,self.sprite.get_height()):
                     if platform.rect.collidepoint(self.pos_x -1, self.pos_y + i):
                         self.pos_x += abs(self.pos_x - (platform.pos_x + platform.width))
                         self.ability_to_move_left = False
@@ -57,23 +68,12 @@ class Player():
             if self.ability_to_move_left == True :
                 self.pos_x -= self.velocity
 
-        
-        if keys[pygame.K_SPACE] or self.is_jumping:
-            if not self.is_jumping and self.jump_ability == True:
+        if keys[pygame.K_SPACE] or self.is_jumping:     #permet de sauter, on verifie si le player est deha en train de sauter afin de continuer ce saut
+            if not self.is_jumping and self.jump_ability == True:   #Si il n'est pas en train de sauter, alors on initialise le saut
                 self.jump_velocity = 10
             self.jump()
 
-        gravity = True
-        for platform in platforms:
-            for i in range (0,50):
-                if platform.rect.collidepoint(self.pos_x + i, self.pos_y + 50) and not self.is_jumping:
-                    gravity = False
-                    self.is_jumping = False
-                    self.jump_ability = True
-                    self.jump_velocity = 0
-                    self.pos_y -= abs(self.pos_y + 50 - platform.pos_y)
-                
-        if gravity == True and not self.is_jumping:
+        if not self.on_floor and not self.is_jumping:
             if self.jump_velocity < 10:
                 self.jump_velocity += 0.2
             self.pos_y += self.jump_velocity
@@ -94,7 +94,6 @@ class Player():
                 pygame.display.update(self.rect)
             else:
                 self.is_jumping = False
-
 
 class Ennemy():
     def __init__(self, damage, pos):
--- Platformer/Level/Generator.py
+++ Platformer/Level/Generator.py
@@ -1,9 +1,12 @@
 import pygame
 from Level.Platforms import *
 
+#Structure des plateformes :
+    #Rect_Platform(screen, size, pos, color)
+
 def launch(screen):
     platforms_list = []
-    platforms_list.append(Rect_Platform(screen, (screen.get_width(), 10), (0,screen.get_height() - 10), (255,255,255)))
+    platforms_list.append(Rect_Platform(screen, (screen.get_width(),10), (0,screen.get_height() - 10), (255,255,255)))
     platforms_list.append(Rect_Platform(screen, (100, 10), (200, screen.get_height()-20), (255,0,0)))
     platforms_list.append(Rect_Platform(screen, (10,10), (1500, 1040), (0,0,255)))
 
